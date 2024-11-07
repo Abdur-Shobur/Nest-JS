1bfb4f3985f626cd7a41cb8637e4a0d603bb355d
abdur-shobur <abdurshobur.developer@gmail.com>
2024-11-07 14:54:06 +0600

    add crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { BaseModule } from './base/base.module';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    BaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
